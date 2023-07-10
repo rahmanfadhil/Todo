391b8df3b1ad1b6b683796eb4f0fa5d01dbcfe78
Abdurrahman Fadhil <rhmnfadhil@gmail.com>
2023-07-10 23:11:19 +0700

    changes

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -8,21 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var newTodoTextField: UITextField!
     
+
+    @IBOutlet weak var listLabel: UILabel!
+    @IBOutlet weak var newTodoTextField: UITextField!
+
     var todos: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
     }
 
     @IBAction func onAddTodo(_ sender: Any) {
         if let todo = newTodoTextField.text {
+            // add todo to the list
             todos.append(todo)
+            
+            // empty the text field
             newTodoTextField.text = ""
+            
+            // display the todo in the ui
+            listLabel.text = todos.joined(separator: "\n")
+
             print(todos)
         }
     }
