d195916e786455cee5a0075614a78c827a97987c
Abdurrahman Fadhil <rhmnfadhil@gmail.com>
2023-07-10 22:26:20 +0700

    Add very very simple todo list

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newTodoTextField: UITextField!
+    
+    var todos: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
 
-
+    @IBAction func onAddTodo(_ sender: Any) {
+        if let todo = newTodoTextField.text {
+            todos.append(todo)
+            newTodoTextField.text = ""
+            print(todos)
+        }
+    }
+    
 }
 
